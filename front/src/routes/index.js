1cf0d3c6fd644e4336e42690606275b7393358a3
Zadigo <pendenquejohn@gmail.com>
2021-10-06 01:11:05 +0200

    Initial commit

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import routesForHome from './home'
 
+import store from '../store'
+
 Vue.use(Router)
 
 var routes = [
@@ -25,12 +27,43 @@ var routes = [
         path: '/payment',
         name: 'restaurant',
         components: { default: () => import(/* webpackChunkName "payment" */ '../views/Payment.vue') }
+    },
+    {
+        path: '/statistics',
+        name: 'statistics',
+        components: { default: () => import(/* webpackChunkName "statistics" */ '../views/Statistics.vue') },
+        meta: {
+            requiresAuthentication: true,
+            requiresAdmin: true
+        }
     }
 ]
 
 var router = new Router({
     mode: 'history',
-    routes: routes
+    routes: routes,
+    scrollBehavior: () => { window.scroll(0, 0) }
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuthentication) {
+        var requiresAdmin = to.meta.requiresAdmin
+        var requiresCashier = to.meta.requiresCashier
+
+        if (store.getters['isAuthenticated']) {
+            if (requiresAdmin & store.getters['isAdmin']) {
+                next()
+            } 
+    
+            if (requiresCashier & store.getters['isCashier']) {
+                next()
+            }
+        }
+
+        next('login')
+    } else {
+        next()
+    }
 })
 
 export default router
